df5e524ad5ce6a4dcaaa3e77f72f550a87581f34
KlariceT <116344288+KlariceT@users.noreply.github.com>
2023-09-20 13:29:05 +0800

    Update role.sql

--- role.sql
+++ role.sql
@@ -1,17 +1,15 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-CREATE DATABASE IF NOT EXISTS `roles` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `roles`;
+CREATE DATABASE IF NOT EXISTS `Role_Skill` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `Role_Skill`;
 
-DROP TABLE IF EXISTS `roles`;
-CREATE TABLE IF NOT EXISTS `roles` (
-    `roleID` int NOT NULL AUTO_INCREMENT,
-    `role_name` varchar(64) NOT NULL,
-    `description` varchar(64) NOT NULL,
-    PRIMARY KEY (`roleID`)
+DROP TABLE IF EXISTS `Role_Skill`;
+CREATE TABLE IF NOT EXISTS `Role_Skill` (
+    `Role_Name` varchar(20) PRIMARY KEY,
+    `Skill_Name` varchar(50) PRIMARY KEY
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
-INSERT INTO `roles` (`roleID`,`role_name`,`description`) VALUES
-(1,`Product Manager`,`In charge of product development`);
-ALTER TABLE roles
+INSERT INTO `Role_Skill` (`roleID`,`role_name`,`description`) VALUES
+(`Product Manager`,`Incharge of product development`);
+ALTER TABLE Role_Skill
