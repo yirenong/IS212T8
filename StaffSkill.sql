0237234b4e3395cbb221b599d92378624b0542c6
KlariceT <116344288+KlariceT@users.noreply.github.com>
2023-09-20 13:31:07 +0800

    Update and rename employeeSkill.sql to StaffSkill.sql

---
+++ StaffSkill.sql
@@ -0,0 +1,21 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+CREATE DATABASE IF NOT EXISTS `Staff_Skill` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `Staff_Skill`;
+
+DROP TABLE IF EXISTS `Staff_Skill`;
+CREATE TABLE IF NOT EXISTS `Staff_Skill` (
+    `Staff_ID` int FOREIGN KEY,
+    `Skill_Name` varchar(20) FOREIGN KEY,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+INSERT INTO `Staff_Skill` (`employeeID`,`skill`) VALUES
+(10001, `testSkill`);
+(10001, `testSkill3`);
+(10002, `testSkill2`);
+(10002, `testSkill4`);
+(10003, `testSkill1`);
+(10003, `testSkill2`);
+(10003, `testSkill3`);
+ALTER TABLE Staff_Skill
