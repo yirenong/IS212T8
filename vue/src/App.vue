96b2ac80b5a468fb101bf9ac44d2a594eb6ab96b
justintew <justinntew@gmail.com>
2023-10-11 00:26:29 +0800

    resolve App.vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div id="app">
     <AppHeader />
-<<<<<<< HEAD
-    <RoleCreationModal />
-=======
     <!-- <RoleCreationModal /> -->
->>>>>>> 867abc6709267d080f8752ed6db60484647ecd7b
     <!-- Use named views to specify where components should be rendered -->
     <router-view></router-view>
     
@@ -15,26 +11,15 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-import AppHeader from './components/HeaderComp.vue';
-import AppFooter from './components/FooterComp.vue';
-import RoleCreationModal from './components/RoleCreationModal.vue';
-=======
 import AppHeader from "./components/HeaderComp.vue";
 import AppFooter from "./components/FooterComp.vue";
 //import RoleCreationModal from "./components/RoleCreationModal.vue";
->>>>>>> 867abc6709267d080f8752ed6db60484647ecd7b
 
 export default {
   components: {
     AppHeader,
     AppFooter,
-<<<<<<< HEAD
-    RoleCreationModal
-  },
-=======
    // RoleCreationModal,
   }
->>>>>>> 867abc6709267d080f8752ed6db60484647ecd7b
 };
 </script>
