bbed793cb9747c1367bd5d1feb0a6f2fb2556b17
Juan <huaan.ang.2021@smu.edu.sg>
2023-10-12 14:13:24 +0800

    Input elements and text search updates
    Created input element for userstory role listing filter.
    
    Finished searching by text and name

--- vue/src/components/JobListing.vue
+++ vue/src/components/JobListing.vue
@@ -1,90 +1,101 @@
 <template>
   <div class="container mt-4">
     <h2>Job Listings</h2>
+    <div class="row mb-4">
+      <div class="col-md-4 ">
+        <label for="date-select">Sort by: </label>
+        <select class="form-control" id="date-select" name="date-select">
+          <option value="latest-date">Latest Date</option>
+          <option value="oldest-date">Oldest Date</option>
+        </select>
+      </div>
+      <div class="col-md-4">
+        <label for="alphabetical-order"></label>
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="alphabetical-order" name="alphabetical-order">
+          <label class="form-check-label" for="alphabetical-order">Ascending Order</label>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <label for="input-text"></label>
+        <input type="text" class="form-control" id="input-text" name="input-text" placeholder="Seach by Name" v-model="searchbyname">
+      </div>
+    </div>
+
     <div v-if="isLoading">
       Loading...
     </div>
     <div v-else>
-    <div class="card mb-4" v-for="(job, index) in jobListings" :key="index">
-      <div class="row">
-        <div class="card-body col-md-8">
-          <div class="m-2">
-            <h4 class="card-title">{{ job.Role.Role_Name }}</h4>
-            <p class="card-text">{{ job.Role.Description }}</p>
-          </div>
-          <div class="col-md-6">
-            <table class="table table-borderless">
-              <tbody>
-                <tr>
-                  <td><b>Number of Openings:</b></td>
-                  <td>{{ job.Opening }}</td>
-                </tr>
-                <tr>
-                  <td><b>Date Posted:</b></td>
-                  <td>{{ job.Date_posted }}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <div class="col-md-6 m-2">
-            <div class="mb-2">
-              <b>Skill(s) Required:</b>
+      <div class="card mb-4" v-for="(job, index) in jobListings" :key="index">
+        <div class="row" v-if="job.Role.Role_Name.toLowerCase().includes(searchbyname.toLowerCase()) ">
+          <div class="card-body col-md-8">
+            <div class="m-2">
+              <h4 class="card-title">{{ job.Role.Role_Name }}</h4>
+              <p class="card-text">{{ job.Role.Description }}</p>
             </div>
-            <div class="skill-icons">
-              <div class="mr-2">
-                <span
-                  class="badge bg-success text-white mr-2 p-2"
-                  v-for="(skill, skillIndex) in job.Role.Skills"
-                  :key="skillIndex"
-                >
-                  {{ skill }}
-                </span>
+            <div class="col-md-6">
+              <table class="table table-borderless">
+                <tbody>
+                  <tr>
+                    <td><b>Number of Openings:</b></td>
+                    <td>{{ job.Opening }}</td>
+                  </tr>
+                  <tr>
+                    <td><b>Date Posted:</b></td>
+                    <td>{{ job.Date_posted }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="col-md-6 m-2">
+              <div class="mb-2">
+                <b>Skill(s) Required:</b>
+              </div>
+              <div class="skill-icons">
+                <div class="mr-2">
+                  <span class="badge bg-success text-white mr-2 p-2" v-for="(skill, skillIndex) in job.Role.Skills"
+                    :key="skillIndex">
+                    {{ skill }}
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-6 m-2">
+              <b>Skill(s) you have:</b>
+              <div class="skill-icons">
+                <div class="mr-2">
+                  <span class="badge bg-success text-white mr-2 p-2" v-for="skill in staffSkills.Skills" :key="skill">
+                    {{ skill }}
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-6 m-2">
+              <b>Skill(s) you don't have:</b>
+              <div class="skill-icons">
+                <div class="mr-2">
+                  <span class="badge bg-danger text-white mr-2 p-2"
+                    v-for="skill in leftoverSkills(job.Role.Skills, staffSkills.Skills)" :key="skill">
+                    {{ skill }}
+                  </span>
+                </div>
               </div>
             </div>
           </div>
 
-          <div class="col-md-6 m-2">
-            <b>Skill(s) you have:</b>
-            <div class="skill-icons">
-              <div class="mr-2">
-                <span
-                  class="badge bg-success text-white mr-2 p-2"
-                  v-for="skill in staffSkills.Skills"
-                  :key="skill"
-                >
-                  {{ skill }}
-                </span>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-6 m-2">
-            <b>Skill(s) you don't have:</b>
-            <div class="skill-icons">
-              <div class="mr-2">
-                <span
-                  class="badge bg-danger text-white mr-2 p-2"
-                  v-for="skill in leftoverSkills(job.Role.Skills, staffSkills.Skills)"
-                  :key="skill"
-                >
-                  {{ skill }}
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        
-        <div class="col-md-4">
-          <div v-if="job.Role && job.Role.Skills && job.Role.Skills.length > 0" class="m-2">
-            <h4>Match Percentage: {{ calculateMatchPercentage(job.Role.Skills) }}%</h4>
+          <div class="col-md-4">
+            <div v-if="job.Role && job.Role.Skills && job.Role.Skills.length > 0" class="m-2">
+              <h4>Match Percentage: {{ calculateMatchPercentage(job.Role.Skills) }}%</h4>
               <DonutChart :percentage="calculateMatchPercentage(job.Role.Skills)" />
-          </div>
-          <div v-else>
-            <b>Match Percentage:</b> Not available
+            </div>
+            <div v-else>
+              <b>Match Percentage:</b> Not available
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   </div>
 </template>
@@ -101,19 +112,20 @@ export default {
   data() {
     return {
       jobListings: [],
-      userData: null, 
-      staffSkills: null, 
+      userData: null,
+      staffSkills: null,
       isLoading: true,
+      searchbyname :'',
     };
   },
   created() {
     if (this.$session.get('user') == null) {
       this.$router.push('/login'); // Redirect to login page
     }
-    else{
+    else {
       this.loadData();
     }
-    
+
   },
   methods: {
     async loadData() {
